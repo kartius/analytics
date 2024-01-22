be74cccfeddddf0307911aff2af17d7a362e389c
MichaelTim75 <141351262+MichaelTim75@users.noreply.github.com>
2024-01-22 19:25:39 +0100

    added scheduled for coinMarketCup and sent it to telegram. (#5)
    3 different tickers and 2 different currencies

--- src/main/java/com/example/analytics/task/GetApiDataTask.java
+++ src/main/java/com/example/analytics/task/GetApiDataTask.java
@@ -1,16 +1,31 @@
 package com.example.analytics.task;
 
+import com.example.analytics.service.coinMarketCap.CryptoPriceService;
+import com.example.analytics.service.telegram.TelegramBotService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class GetApiDataTask {
 
+    private static final List<String> TICKERS = List.of("BTC", "USDT", "ETH");
+    private static final List<String> CURRENCIES = List.of("USD", "EUR");
+
+    private final CryptoPriceService cryptoPriceService;
+    private final TelegramBotService telegramBotService;
 
     @Scheduled(cron = "0 0 4,8,16,20 * * *")
-    public void GetSomeApiDataTask(){
-        log.info("Get some data - mock for task in future");
+    public void getApiDataTask() {
+        log.info("Starting task for get prices");
+
+        TICKERS.forEach(t ->
+                CURRENCIES.forEach(c -> telegramBotService.broadcast(cryptoPriceService.getCryptoPrice(t, c)))
+        );
     }
 }
